c771018f0248284a5d5a33d47480a7934923cc59
Dave Lake <dclake@gmail.com>
2021-01-29 09:50:24 -0400

    Changed formatting on Readme

--- seMethodsProject/src/main/java/com/napier/sem/App.java
+++ seMethodsProject/src/main/java/com/napier/sem/App.java
@@ -1,2 +1,9 @@
-package com.napier.sem;public class App {
+package com.napier.sem;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Boo yah!");
+    }
 }
