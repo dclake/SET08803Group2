86d5ea804470c4e918a7f20971a8f4159e413794
Dave Lake <dclake@gmail.com>
2021-02-07 14:37:09 -0400

    Changed app output

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        System.out.println("SeMethods Assignment - Group 2");
     }
 }
