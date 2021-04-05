38acd629e52f403e97ed9678f05442201fb10a56
dave.lake <dclake@gmail.com>
2021-04-04 23:05:12 -0400

    Added City Class

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,21 @@
+package com.napier.sem;
+
+public class City {
+    /**
+     * City Name
+     */
+    public String City_Name;
+    /**
+     * City Name
+     */
+    public Country country_name;
+    /**
+     * District
+     */
+    public String District;
+    /**
+     * Population
+     */
+    public int Population;
+
+}
